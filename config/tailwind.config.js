4f0dd19b6a025310c3edc573a9d3322aa2f4e3b6
Mark Daniel Asiddao <markasiddao@gmail.com>
2024-12-04 19:18:49 +0800

    Instaled Tailwind and added Styling

---
+++ config/tailwind.config.js
@@ -0,0 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Helvetica', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'terminus-blue': '#466FC8',
+        'custom-red': '#EF3366'
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
